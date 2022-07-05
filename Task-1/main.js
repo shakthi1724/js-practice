2823d4e0dadf797480c3a3c5a2d5eb2ec044fdd4
siva <sivashakthiarun.c@gmail.com>
2022-07-05 17:09:33 +0530

    add task1

---
+++ Task-1/main.js
@@ -0,0 +1,10 @@
+var a=100;
+var b=10;
+var c=a+b;
+var d=a-b;
+var e=a*b;
+var f=a/b;
+console.log("The Addition is:"+c)
+console.log("The Subtraction is:"+d)
+console.log("The Multiplication is:"+e)
+console.log("The Division is:"+f)
